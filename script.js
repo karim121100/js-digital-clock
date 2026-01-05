0cfd65e2754273732b839031dad3eea4539aa6be
KB <161012310+karim121100@users.noreply.github.com>
2026-01-05 22:50:27 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function updateClock() {
+  const now = new Date();
+
+  let h = String(now.getHours()).padStart(2, '0');
+  let m = String(now.getMinutes()).padStart(2, '0');
+  let s = String(now.getSeconds()).padStart(2, '0');
+
+  document.getElementById("clock").textContent = `${h}:${m}:${s}`;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
